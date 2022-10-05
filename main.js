8da5d310adf61c16b7b2460d91dcd4b5ae759616
alex <alexhalpin0@gmail.com>
2022-10-05 08:50:14 -0400

    added blackboard

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Canvas Init
   /** @type {HTMLCanvasElement} */
   const canvas = document.getElementById("Canvas");
   /** @type {CanvasRenderingContext2D} */
@@ -10,24 +11,45 @@ document.addEventListener("DOMContentLoaded", () => {
   ctx.lineWidth = 5;
   ctx.lineCap = "round";
 
+  // Prevent on Clickable click
+  document.querySelectorAll(".Clickable").forEach((element) => {
+    element.addEventListener("mouseover", () => {
+      document.removeEventListener("mousedown", startStroke);
+    });
+    element.addEventListener("mouseleave", () => {
+      document.addEventListener("mousedown", startStroke);
+    });
+  });
+
+  // Resize Canvas
   document.addEventListener("resize", () => {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
   });
 
+  // Reset Button
+  const resetButton = document.getElementById("Reset");
+  resetButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  // Drawing functions
   function drawStroke(event) {
     ctx.lineTo(event.clientX, event.clientY);
     ctx.stroke();
   }
 
   function startStroke(event) {
+    if (hoveringButton) return;
     ctx.beginPath();
     ctx.moveTo(event.clientX, event.clientY);
+    ctx.lineTo(event.clientX, event.clientY);
+    ctx.stroke();
     document.addEventListener("mousemove", drawStroke);
   }
 
+  // Drawing Event Listeners
   document.addEventListener("mousedown", startStroke);
-
   document.addEventListener("mouseup", () => {
     document.removeEventListener("mousemove", drawStroke);
   });
