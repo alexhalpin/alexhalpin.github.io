ec22b805d39eb32527c4bac572e1a0d759919d16
alex <alexhalpin0@gmail.com>
2024-03-31 17:07:15 -0400

    poster fade, tidiness

---
+++ helpers.js
@@ -0,0 +1,84 @@
+import { streamLinks } from "./links.js";
+
+export function setStreamSize(player, height, width) {
+  if (player == undefined) {
+    return;
+  }
+
+  var surfcam = document.getElementById("surfcam");
+  var streamTag = document.getElementById("streamTag");
+  var poster = document.getElementById("poster");
+  //   console.log(width / height > 16 / 9);
+
+  var top, left;
+  var streamHeight, streamWidth;
+
+  // portrait
+  if (width / height < 16 / 9) {
+    // console.log("portrait");
+    streamHeight = height;
+    streamWidth = Math.ceil((height * 16) / 9);
+
+    top = 0;
+    left = Math.ceil(0.5 * (width - streamWidth));
+  }
+
+  // landscape
+  else {
+    // console.log("landscape");
+    streamHeight = Math.ceil((width * 9) / 16);
+    streamWidth = width;
+
+    top = Math.ceil((1 / 2) * (height - streamHeight));
+    left = 0;
+  }
+
+  player.height(`${streamHeight}px`);
+  player.width(`${streamWidth}px`);
+
+  surfcam.style.top = `${top}px`;
+  surfcam.style.left = `${left}px`;
+
+  poster.style.height = `${streamHeight}px`;
+  poster.style.width = `${streamWidth}px`;
+  poster.style.top = `${top}px`;
+  poster.style.left = `${left}px`;
+
+  if (width / height < 1) {
+    streamTag.style.right = "";
+  } else {
+    streamTag.style.right = "0px";
+  }
+}
+
+export function getStreamLink() {
+  // get a valid stream link
+  let validStreamLink;
+  for (const [site, links] of Object.entries(streamLinks)) {
+    if (validStreamLink != undefined) {
+      break;
+    }
+    for (const link of links) {
+      if (checkStreamLink(link)) {
+        validStreamLink = link;
+        console.log(`Using ${site} : ${link}`);
+        break;
+      }
+    }
+  }
+
+  return validStreamLink;
+}
+
+async function checkStreamLink(streamLink) {
+  const resp = await fetch(streamLink, { method: "HEAD" });
+  return resp.ok;
+}
+
+export function initLiveUI() {
+  var liveIcon = document.querySelector("#liveIcon");
+  liveIcon.classList.remove("hidden");
+
+  var poster = document.getElementById("poster");
+  poster.style.opacity = 0;
+}
--- main.js
+++ main.js
@@ -1,108 +1,19 @@
-import { streamLinks } from "./links.js";
+import { getStreamLink, initLiveUI, setStreamSize } from "./helpers.js";
 
-console.log("hi :)");
-
-var validStreamLink;
-
-// get a valid stream link
-for (const [site, links] of Object.entries(streamLinks)) {
-  if (validStreamLink != undefined) {
-    break;
-  }
-  for (const link of links) {
-    if (checkStreamLink(link)) {
-      validStreamLink = link;
-      console.log(`Using ${site} : ${link}`);
-      break;
-    }
-  }
-}
-
-let width = window.innerWidth;
-let height = window.innerHeight;
+console.log("🐸");
 
 var player = videojs("surfcam");
-player.src(validStreamLink);
+player.src(getStreamLink());
 player.on("loadeddata", () => {
   console.log("video loaded");
 
   player.play();
-  showLiveTag();
+  initLiveUI();
 });
 
-updateStreamDims(width, height);
-
-window.addEventListener("resize", (event) => {
-  let width = event.target.innerWidth;
-  let height = event.target.innerHeight;
-  updateStreamDims(width, height);
+setStreamSize(player, window.innerHeight, window.innerWidth);
+window.addEventListener("resize", () => {
+  let height = window.innerHeight;
+  let width = window.innerWidth;
+  setStreamSize(player, height, width);
 });
-
-function setStreamSize(width, height) {
-  if (player == undefined) {
-    return;
-  }
-  var surfcam = document.getElementById("surfcam");
-  var streamTag = document.getElementById("streamTag");
-  //   console.log(width / height > 16 / 9);
-
-  var top;
-  var left;
-
-  // portrait
-  if (width / height < 16 / 9) {
-    // console.log("portrait");
-    top = 0;
-    let streamWidth = (height * 16) / 9;
-    left = 0.5 * (width - streamWidth);
-
-    left = Math.ceil(left);
-    streamWidth = Math.ceil(streamWidth);
-
-    surfcam.style.top = `${top}px`;
-    surfcam.style.left = `${left}px`;
-    player.width(`${streamWidth}px`);
-    player.height(`${height}px`);
-  }
-
-  // landscape
-  else {
-    // console.log("landscape");
-    left = 0;
-    let streamHeight = (width * 9) / 16;
-    top = (1 / 2) * (height - streamHeight);
-
-    top = Math.ceil(top);
-    streamHeight = Math.ceil(streamHeight);
-
-    player.width(`${width}px`);
-    player.height(`${streamHeight}px`);
-    surfcam.style.top = `${top}px`;
-    surfcam.style.left = `${left}px`;
-  }
-
-  if (width / height < 1) {
-    streamTag.style.right = "";
-  } else {
-    streamTag.style.right = "0px";
-  }
-}
-
-function updateStreamDims(width, height) {
-  setStreamSize(width, height);
-}
-
-function showLiveTag() {
-  var liveIcon = document.querySelector("#liveIcon");
-  liveIcon.classList.remove("hidden");
-}
-
-// function debug(text) {
-//   debugElement = document.getElementById("debug");
-//   debugElement.innerText = text;
-// }
-
-async function checkStreamLink(streamLink) {
-  const resp = await fetch(streamLink, { method: "HEAD" });
-  return resp.ok;
-}
